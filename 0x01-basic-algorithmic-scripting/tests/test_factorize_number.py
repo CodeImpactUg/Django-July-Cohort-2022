b713f50e084cddf2912c8eddec32f46294b2397e
patrickcmd <pwalukagga@gmail.com>
2022-08-06 10:04:11 +0300

    Implements factorialize number

--- 0x01-basic-algorithmic-scripting/tests/test_factorize_number.py
+++ 0x01-basic-algorithmic-scripting/tests/test_factorize_number.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-from factorize_number import factorize
+from factorize_number import factorialize
 
 
 class TestFactorizeNumber(unittest.TestCase):
@@ -13,7 +13,10 @@ class TestFactorizeNumber(unittest.TestCase):
         factorialize(20) should return 2432902008176640000.
         factorialize(0) should return 1.
         """
-        self.assertEqual("foo".upper(), "FOO")
+        self.assertEqual(factorialize(5), 120)
+        self.assertEqual(factorialize(10), 3628800)
+        self.assertEqual(factorialize(20), 2432902008176640000)
+        self.assertEqual(factorialize(0), 1)
 
 
 if __name__ == "__main__":
