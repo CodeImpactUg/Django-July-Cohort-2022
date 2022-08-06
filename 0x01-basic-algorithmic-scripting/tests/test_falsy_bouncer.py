973357b0f739c594a28ecff3dd131e1c429df51a
patrickcmd <pwalukagga@gmail.com>
2022-08-06 09:58:17 +0300

    Implements falsy bouncer

--- 0x01-basic-algorithmic-scripting/tests/test_falsy_bouncer.py
+++ 0x01-basic-algorithmic-scripting/tests/test_falsy_bouncer.py
@@ -7,12 +7,15 @@ from falsy_bouncer import bouncer
 class TestBouncer(unittest.TestCase):
     def test_bouncer(self):
         """
-        bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
+        bouncer([7, "ate", "", False, 9]) should return [7, "ate", 9].
         bouncer(["a", "b", "c"]) should return ["a", "b", "c"].
-        bouncer([false, null, 0, NaN, undefined, ""]) should return [].
-        bouncer([null, NaN, 1, 2, undefined]) should return [1, 2].
+        bouncer([False, None, 0, None, None, ""]) should return [].
+        bouncer([None, None, 1, 2, None]) should return [1, 2].
         """
-        self.assertEqual("foo".upper(), "FOO")
+        self.assertEqual(bouncer([7, "ate", "", False, 9]), [7, "ate", 9])
+        self.assertEqual(bouncer(["a", "b", "c"]), ["a", "b", "c"])
+        self.assertEqual(bouncer([False, None, 0, None, None, ""]), [])
+        self.assertEqual(bouncer([None, None, 1, 2, None]), [1, 2])
 
 
 if __name__ == "__main__":
