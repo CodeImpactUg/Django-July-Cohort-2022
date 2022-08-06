38a93a5f3b2f4c80d5f05b5a15fb73faf10789eb
patrickcmd <pwalukagga@gmail.com>
2022-08-06 08:55:40 +0300

    Implements longest word

--- 0x01-basic-algorithmic-scripting/longest_word.py
+++ 0x01-basic-algorithmic-scripting/longest_word.py
@@ -7,7 +7,11 @@ Your response should be a number.
 
 
 def findLongestWordLength(st):
-    return len(st)
+    max_word_length = len(st[0])
+    for word in st.split():
+        if len(word) > max_word_length:
+            max_word_length = len(word)
+    return max_word_length
 
 
 if __name__ == "__main__":
