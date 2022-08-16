8f7bb5e8448461f6c71b304a2613a814782d6874
patrickcmd <pwalukagga@gmail.com>
2022-08-16 20:02:28 +0300

    OOP

---
+++ 0x01-vanilla_python/oop/class_exercise.py
@@ -0,0 +1,8 @@
+'''
+Write a program to simulate banking operations by a bank customer
+
+Define Custom class
+Simulate bank deposits
+Simulate bank withdraws (check that you have enough balance amount)
+print receipt
+'''
